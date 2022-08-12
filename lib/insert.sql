cb34fd7fb9f3cf5882979c6c81dc5c1926dfda01
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-12 08:01:26 -0500

    commit tree f1c132c1d805516524fc12d9d5e93ef4f9314787

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age , sex , color , temperament, alive) VALUES ('Mr. Chocolate', 12 , 'M', 'Brown' , 'Friendly', TRUE );
+INSERT INTO bears (id, name, age , sex , color , temperament, alive) VALUES ('Rowdy', 10 , 'F', 'Dark' , 'Friendly', TRUE );
+INSERT INTO bears (id, name, age , sex , color , temperament, alive) VALUES ('Tabitha', 8 , 'M', 'White' , 'Friendly', FALSE );
+INSERT INTO bears (id, name, age , sex , color , temperament, alive) VALUES ('Sergeant Brown', 6 , 'M', 'Brown' , 'Friendly', TRUE );
+INSERT INTO bears (id, name, age , sex , color , temperament, alive) VALUES ('Melissa', 5 , 'F', 'Dark' , 'Friendly', TRUE );
+INSERT INTO bears (id, name, age , sex , color , temperament, alive) VALUES ('Grinch', 12 , 'M', 'Red' , 'Friendly', FALSE );
+INSERT INTO bears (id, name, age , sex , color , temperament, alive) VALUES ('Wendy', 4 , 'F', 'Brown' , 'Friendly', TRUE );
+INSERT INTO bears (id, name, age , sex , color , temperament, alive) VALUES ( NULL , 5 , 'M', 'Brown' , 'Friendly', FALSE );
